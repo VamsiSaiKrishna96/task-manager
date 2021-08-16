f44e7769aa9b313f59dc0acce772d4ae1cc1ba32
vamsi sai krishna <vamsisaikrishna.y@gmail.com>
2021-08-16 11:37:31 +0530

    removed commit

--- src/components/action-buttons/ActionButtons.js
+++ src/components/action-buttons/ActionButtons.js
@@ -2,7 +2,6 @@ import "./ActionButtons.css";
 
 const ActionButtons = () => {
   return (
-    // added commit
     <div className="actions-container">
       <button className="add-button">Add</button>
     </div>
