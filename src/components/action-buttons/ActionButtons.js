cb486d94ecee61a4050a237e3bb49bc38f61243e
vamsi sai krishna <vamsisaikrishna.y@gmail.com>
2021-08-16 11:11:18 +0530

    husky, htmlhint, eslint, prettier

--- src/components/action-buttons/ActionButtons.js
+++ src/components/action-buttons/ActionButtons.js
@@ -2,8 +2,7 @@ import "./ActionButtons.css";
 
 const ActionButtons = () => {
   return (
-    <div className="actions-container">
-      <button className="add-button">Add</button>
+    <div className="actions-container"><button className="add-button">Add</button>
     </div>
   );
 };
