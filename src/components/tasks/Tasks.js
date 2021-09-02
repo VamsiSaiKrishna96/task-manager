9988284d208dc29d9485a1c0f3c2183f72b69be9
vamsi sai krishna <vamsisaikrishna.y@gmail.com>
2021-09-02 15:18:01 +0530

    test

--- src/components/tasks/Tasks.js
+++ src/components/tasks/Tasks.js
@@ -2,6 +2,7 @@ import Task from "../task/task";
 import "./Tasks.css";
 const Tasks = ({ tasks, removeFromList, setAsChecked, isAddingTask }) => {
   return (
+    // if we have tasks rendering tasks if not displaying message 
     <>
       {tasks && tasks?.length ? (
         tasks.map((task) => (
